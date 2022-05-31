5b3f1985321f796ee319500df88761f9a1b6dc03
Basil Stotz <stotz.basil@opinsys-q0AXi4JA>
2022-05-31 15:18:18 +0200

    fix typo

--- server/make-server-template.sh
+++ server/make-server-template.sh
@@ -2,17 +2,9 @@
 
 device="wg1"
 port=41194
-adddress="172.30.0.1"
+address="172.30.0.1"
 netmask=24
 
-if ! test -d keys; then
-    echo "info: generate all keys"
-    umask 077
-    mkdir -p keys
-    wg genkey | tee keys/private | wg pubkey > keys/public
-else
-    echo "info: keys already exist. i keep them"
-fi
 
 cat <<EOF > $device.conf.template 
 [Interface]
